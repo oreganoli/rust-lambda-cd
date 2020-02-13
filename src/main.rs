23ed92d8bb5b007362ad48863d1e87e52122f599
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-02-13 15:14:59 +0100

    wrote a function name extractor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#![feature(str_strip)]
+
+mod test;
+/// Extracts the name of the Lambda function being updated from the code zip's full path and filename.
+fn bare_name(path: &str) -> Option<String> {
+    // explicit type needed here because CLion can't handle Splits
+    let x: Option<&str> = path.split("/").into_iter().last();
+    x
+        .and_then(|s| s.strip_suffix(".zip"))
+        .map(|s| s.to_owned())
+}
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/test.rs
@@ -0,0 +1,9 @@
+#[test]
+fn test_filename_extraction() {
+    use super::bare_name;
+    assert_eq!(Some("my_function".to_owned()), bare_name("Development/releases/my_function.zip"));
+    assert_eq!(Some("other Func_1.0.0.23".to_owned()), bare_name("other Func_1.0.0.23.zip"));
+    assert_eq!(Some("chrząszcz_game_0.32.2-x86_64-linux".to_owned()), bare_name("/dir/subdir/Sub sub dir/chrząszcz_game_0.32.2-x86_64-linux.zip"));
+    assert_eq!(None, bare_name("Docs/not_a_zip.xls"));
+    assert_eq!(None, bare_name("also not a zip"));
+}
