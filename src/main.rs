95f3ff57f712557adcf594e7323ea3e198bfc270
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-02-13 16:05:48 +0100

    started writing handlers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![feature(str_strip)]
 use rusoto_lambda::{LambdaClient, GetFunctionRequest, GetFunctionError, GetFunctionResponse, Lambda};
 use rusoto_core::{HttpClient, RusotoError, credential::{EnvironmentProvider, ProvideAwsCredentials}, Region};
+use std::convert::TryFrom;
+use std::str::FromStr;
+
+
 mod test;
 /// Extracts the name of the Lambda function being updated from the code zip's full path and filename.
 fn bare_name(path: &str) -> Option<String> {
@@ -28,6 +32,35 @@ async fn function_exists(name: &str, client: &LambdaClient) -> Result<bool, Ruso
         }
     }
 }
+
+async fn handler() -> Result<(), ()> {
+    // Which bucket to watch. Mandatory.
+    let bucket = match std::env::var("BUCKET") {
+        Ok(s) => s,
+        Err(_) => return Err(())
+    };
+    // AWS region. Also mandatory.
+    let region = match std::env::var("AWS_REGION") {
+        Ok(s) => match Region::from_str(&s) {
+            Ok(reg) => reg,
+            Err(_) => {
+                eprintln!("Could not parse the given AWS region.");
+                return Err(())
+            }
+        },
+        Err(_) => return Err(())
+    };
+    // Which functions to auto-update. If not provided, will try to update every .zip matching the name of a function.
+    let monitored_names = std::env::var("FUNCTION_NAMES").ok();
+    // Which folder to watch for changes in. If not provided, all new .zips will be checked.
+    let monitored_folder = std::env::var("DIRECTORY").ok();
+    Ok(())
+}
+
+fn sync_wrapper(lambda_result: Result<(), ()>) -> Result<(), ()> {
+    lambda_result
+}
+
 #[tokio::main]
 async fn main() {
     let lambda_client = LambdaClient::new_with(
